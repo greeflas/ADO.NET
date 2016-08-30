612cfca06bd4ea22674ac38a95e690182252bbb4
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-08-30 10:21:25 +0300

    Connect to DB

---
+++ lesson1/ConnectionString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ConnectionString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString =
+                @"Data source = localhost;
+                Initial Catalog = ShopAdo;
+                Integrated Security = false;
+                user = sa;
+                password = student;
+                Connect Timeout = 15;";
+
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Success!");
+            }
+            catch(Exception error)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("[database]: {0}", error.Message);
+            }
+            finally
+            {
+                if(connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+        }
+    }
+}
