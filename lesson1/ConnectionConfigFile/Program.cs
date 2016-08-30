641af57ec1ef9d51b3804d12c20175c554310040
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-08-30 11:12:34 +0300

    DB connection data from XML file

---
+++ lesson1/ConnectionConfigFile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ConnectionConfigFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["shop"].ConnectionString;
+            SqlConnection connection =  new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Success!");
+            }
+            catch (Exception error)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("[database]: {0}", error.Message);
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+        }
+    }
+}
---
+++ lesson1/ConnectionConfigFileWithoutUserData/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ConnectionConfigFileWithoutUserData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login = string.Empty;
+            string password = string.Empty;
+
+            Console.Write("Input login: ");
+            login = Console.ReadLine();
+
+            Console.Write("Input password: ");
+            password = Console.ReadLine();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["shop"].ConnectionString;
+            SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder();
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Success!");
+            }
+            catch (Exception error)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("[database]: {0}", error.Message);
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+        }
+    }
+}
