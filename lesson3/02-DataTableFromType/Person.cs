ec034f096ff1df8ce4444fbe01b2254200dde319
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-09-06 11:49:07 +0300

    DataTable from class

---
+++ lesson3/02-DataTableFromType/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data;
+
+namespace _02_DataTableFromType
+{
+    class Person
+    {
+        int id;
+        string first_name;
+        string last_name;
+        DateTime birth_date;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string FirstName
+        {
+            get { return first_name; }
+            set { first_name = value; }
+        }
+
+        public string LastName
+        {
+            get { return last_name; }
+            set { last_name = value; }
+        }
+
+        public DateTime Birth_date
+        {
+            get { return birth_date; }
+            set { birth_date = value; }
+        }
+
+        public static DataTable createIdentity()
+        {
+            // Create table with fields
+            DataTable person = new DataTable("person");
+
+            DataColumn id = new DataColumn("id", typeof(int));
+            id.Caption = "Peson id";
+            id.ReadOnly = true;
+            id.AllowDBNull = false;
+            id.Unique = true;
+            id.AutoIncrement = true;
+            id.AutoIncrementStep = 1;
+            id.AutoIncrementSeed = 1;
+
+            DataColumn FirstName = new DataColumn("first_name", typeof(string));
+            FirstName.MaxLength = 255;
+
+            DataColumn LastName = new DataColumn("last_name", typeof(string));
+            LastName.MaxLength = 255;
+
+            DataColumn BirthDate = new DataColumn("birth_date", typeof(DateTime));
+
+            person.Columns.AddRange(new DataColumn[] { id, FirstName, LastName, BirthDate });
+
+            return person;
+        }
+    }
+}
---
+++ lesson3/02-DataTableFromType/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+
+namespace _02_DataTableFromType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable identity = Person.createIdentity();
+
+            // Person 1
+            DataRow person1 = identity.NewRow();
+            person1[1] = "Vasya";
+            person1[2] = "Pupkin";
+            person1[3] = new DateTime(2000, 3, 8);
+
+            identity.Rows.Add(person1);
+
+            // Person 2
+            DataRow person2 = identity.NewRow();
+            person2[1] = "Sveta";
+            person2[2] = "Pupkina";
+            person2[3] = new DateTime(1996, 7, 1);
+
+            identity.Rows.Add(person2);
+
+            foreach(DataRow row in identity.Rows)
+            {
+                Console.WriteLine("{0} {1}", row[1], row[2]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
