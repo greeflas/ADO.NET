e6f669f65440a69083a6fbcdc013a6164eb85987
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-09-08 11:56:49 +0300

    WinForm example

---
+++ lesson4/02-WinFormExample/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace _02_WinFormExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewCategory = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCategory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewCategory
+            // 
+            this.dataGridViewCategory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCategory.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewCategory.Name = "dataGridViewCategory";
+            this.dataGridViewCategory.Size = new System.Drawing.Size(260, 202);
+            this.dataGridViewCategory.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 227);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridViewCategory);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AdoNET";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCategory)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewCategory;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ lesson4/02-WinFormExample/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace _02_WinFormExample
+{
+    public partial class Form1 : Form
+    {
+        string connectionStr;
+        SqlConnection connect;
+
+        SqlDataAdapter adapter;
+        SqlCommandBuilder builder;
+        DataTable Category;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            connectionStr = ConfigurationManager.ConnectionStrings["shop"].ConnectionString;
+            connect = new SqlConnection(connectionStr);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            adapter = new SqlDataAdapter("SELECT * FROM dbo.Category", connect);
+            adapter.RowUpdated += Adapter_RowUpdated;
+
+            builder = new SqlCommandBuilder(adapter);
+
+            adapter.DeleteCommand = builder.GetDeleteCommand();
+            adapter.InsertCommand = builder.GetInsertCommand();
+            adapter.UpdateCommand = builder.GetUpdateCommand(true);
+
+            Category = new DataTable("Category");
+            adapter.Fill(Category);
+
+            dataGridViewCategory.DataSource = Category;
+        }
+
+        private void Adapter_RowUpdated(object sender, SqlRowUpdatedEventArgs e)
+        {
+            if(e.Errors == null && e.StatementType == StatementType.Insert)
+            {
+                // IDENT_CURRENT('table_name') --all sessions
+                // SCOPE_IDENTITY() --current session
+                string query = "SELECT CategoryId FROM dbo.Category WHERE CategoryId = IDENT_CURRENT('dbo.Category')";
+                SqlCommand cmd = new SqlCommand(query, connect);
+
+                if(connect.State != ConnectionState.Open)
+                    connect.Open();
+
+                int id = (int)cmd.ExecuteScalar();
+
+                e.Row["CategoryId"] = id;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            adapter.Update(Category);
+            MessageBox.Show("Change saved!", "Database", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
