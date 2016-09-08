381a92116c0ebba2e97be924cd64fce76287014b
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-09-08 10:23:42 +0300

    DataAdapter

---
+++ lesson4/01-DataAdapter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace _01_DataAdapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionStr = ConfigurationManager.ConnectionStrings["shop"].ConnectionString;
+            SqlConnection connect = new SqlConnection(connectionStr);
+
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM dbo.Category", connect);
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+
+            adapter.DeleteCommand = builder.GetDeleteCommand();
+            adapter.InsertCommand = builder.GetInsertCommand();
+            adapter.UpdateCommand = builder.GetUpdateCommand(true);
+
+            Console.WriteLine("Delete - {0}\n", adapter.DeleteCommand.CommandText);
+            Console.WriteLine("Insert - {0}\n", adapter.InsertCommand.CommandText);
+            Console.WriteLine("Update - {0}\n", adapter.UpdateCommand.CommandText);
+
+            DataTable Category = new DataTable("Category");
+            adapter.Fill(Category);
+
+            foreach (DataColumn col in Category.Columns)
+                Console.Write("{0}\t", col.ColumnName);
+
+            Console.WriteLine("\n----------------------------");
+
+            foreach (DataRow row in Category.Rows)
+                Console.WriteLine("{0}\t{1}", row[0], row[1]);
+        }
+    }
+}
