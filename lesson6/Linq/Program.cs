5396ecf2b2fde9d57fef2c31053abf1c469de232
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-09-20 09:57:46 +0300

    LINQ

---
+++ lesson6/Linq/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = { "Mercedez", "Aston Martin", "BMW", "Ford", "Audi" };
+
+            /* Lambda */
+            var query = cars.Where(car => car.Length == 4);
+            foreach (var item in query)
+                Console.WriteLine(item);
+
+            /* Classic */
+            var query2 = from car in cars
+                         where car.Length == 4
+                         select car;
+            foreach (var item in query2)
+                Console.WriteLine(item);
+
+            Console.WriteLine("\tTwo 'o' letters");
+            var query3 = cars.Where(car => car.ToLower().Count(x => x == 'a') == 2);
+            foreach (var item in query3)
+                Console.WriteLine(item);
+
+            Console.WriteLine("\n\tAnonym object");
+            var query4 = cars.Where(car => car.Length < 5)
+                        .Select(car => new { car, car.Length });
+            foreach (var item in query4)
+                Console.WriteLine(item);
+        }
+    }
+}
---
+++ lesson6/Yield/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+namespace Yield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
