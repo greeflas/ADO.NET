1fc90aa167dfd2ea972c09ad58004a0c3d0b2630
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-09-22 10:49:24 +0300

    LINQ: Pagination

---
+++ lesson7/02-Pagination/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using DataLayer;
+
+namespace _02_Pagination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<EmployeeExt> empext = DataManager.GetEmployeeExt();
+
+            var pages = empext.Skip(4).Take(4);
+
+            foreach (var item in pages)
+                Console.WriteLine(item);
+
+            Console.WriteLine();
+        }
+    }
+}
